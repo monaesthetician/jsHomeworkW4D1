2029e645f5a9fbd6ec41025b51c66c503f032e13
Mona <emonearth915@gmail.com>
2021-04-19 21:48:53 -0400

    ja homework week4 day1

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// Chalenge 1
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+// Chalenge 2
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var i = 5;
+while(i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// chalenge 3
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+// chalenge 4
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 9; i > 0; i -=2) {
+  myArray.push(i);
+}
